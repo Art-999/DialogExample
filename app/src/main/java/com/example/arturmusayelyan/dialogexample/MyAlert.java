5d9715fafca4826fceb283884284691abf1bd607
artur.musayelyan <musayelyan099@gmail.com>
2017-12-26 14:46:11 +0400

    26.12.2017 ArtCommit gorcic ( 1 )

--- app/src/main/java/com/example/arturmusayelyan/dialogexample/MyAlert.java
+++ app/src/main/java/com/example/arturmusayelyan/dialogexample/MyAlert.java
@@ -2,8 +2,10 @@ package com.example.arturmusayelyan.dialogexample;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
 
 /**
  * Created by artur.musayelyan on 25/12/2017.
@@ -24,6 +26,35 @@ public class MyAlert extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        //setCancelable(false);
+        builder.setTitle("My Dialog");
+       // builder.setMessage("Do you like this dialog?");
+        builder.setMultiChoiceItems(R.array.days, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                Toast.makeText(getActivity(), "item was selected "+which+" was selected "+isChecked, Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        //List e avelasnum
+//        builder.setItems(R.array.days, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                Toast.makeText(getActivity(), "item was selected "+which, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "Negative button was clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "Positive button was clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
         Dialog dialog = builder.create();
 
         return dialog;
